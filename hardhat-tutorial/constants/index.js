8e7828835eccae0854e1e9ce9fabee1bdd7d7def
Daylinn <dhart5@students.asurams.edu>
2022-08-24 15:50:11 -0400

    Add existing project files to Git

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0xB3ad0aD84657BA91ea77177944B28dD81E0117C8";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
